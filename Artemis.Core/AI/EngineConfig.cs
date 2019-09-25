80b889a08cf76a984db0c8622fa9a1450b667548
Roy Arad <royar13@gmail.com>
2019-09-25 17:41:44 +0300

    Add license

---
+++ Artemis.Core/AI/EngineConfig.cs
@@ -0,0 +1,16 @@
+﻿using Artemis.Core.AI.Evaluation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Artemis.Core.AI
+{
+    public class EngineConfig
+    {
+        public int InitialAlpha = -PositionEvaluator.CHECKMATE_SCORE * 2;
+        public int InitialBeta = PositionEvaluator.CHECKMATE_SCORE * 2;
+        public bool ConstantDepth = true;
+        public int Depth = 5;
+        public int MaxDepth = 10;
+    }
+}
